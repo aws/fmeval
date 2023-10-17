591acb5cfab3fa7fdb655f12cb027a90743c480a
Aman Malhotra <amamalh@amazon.com>
2023-10-16 21:42:57 -0700

    feat: implement summarization accuracy semantic robustness eval algo

--- src/amazon_fmeval/eval_algo_mapping.py
+++ src/amazon_fmeval/eval_algo_mapping.py
@@ -3,10 +3,14 @@ from typing import Dict, Type
 from amazon_fmeval.eval_algorithms import EvalAlgorithm
 from amazon_fmeval.eval_algorithms.eval_algorithm import EvalAlgorithmInterface
 from amazon_fmeval.eval_algorithms.factual_knowledge import FactualKnowledge
+from amazon_fmeval.eval_algorithms.general_semantic_robustness import GeneralSemanticRobustness
 from amazon_fmeval.eval_algorithms.prompt_stereotyping import PromptStereotyping
 from amazon_fmeval.eval_algorithms.qa_accuracy import QAAccuracy
 from amazon_fmeval.eval_algorithms.summarization_accuracy import SummarizationAccuracy
 from amazon_fmeval.eval_algorithms.classification_accuracy import ClassificationAccuracy
+from amazon_fmeval.eval_algorithms.summarization_accuracy_semantic_robustness import (
+    SummarizationAccuracySemanticRobustness,
+)
 from amazon_fmeval.exceptions import EvalAlgorithmClientError
 
 EVAL_ALGORITHMS: Dict[str, Type["EvalAlgorithmInterface"]] = {
@@ -15,6 +19,8 @@ EVAL_ALGORITHMS: Dict[str, Type["EvalAlgorithmInterface"]] = {
     EvalAlgorithm.SUMMARIZATION_ACCURACY.value: SummarizationAccuracy,
     EvalAlgorithm.PROMPT_STEREOTYPING.value: PromptStereotyping,
     EvalAlgorithm.CLASSIFICATION_ACCURACY.value: ClassificationAccuracy,
+    EvalAlgorithm.GENERAL_SEMANTIC_ROBUSTNESS.value: GeneralSemanticRobustness,
+    EvalAlgorithm.SUMMARIZATION_ACCURACY_SEMANTIC_ROBUSTNESS.value: SummarizationAccuracySemanticRobustness,
 }
 
 
