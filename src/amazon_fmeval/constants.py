7e5ffcd98ddd484df210b26c0654257bd8ad7dbb
keerthanvasist <kvasist@amazon.com>
2023-10-23 12:34:17 -0700

    fix: fix pipeline: set MAX_ROWS_TO_TAKE to 100000 (#72)

--- src/amazon_fmeval/constants.py
+++ src/amazon_fmeval/constants.py
@@ -66,4 +66,4 @@ PREFIX_FOR_DELTA_SCORES = "delta_"
 # Check if model is deterministic for first NUM_ROWS_DETERMINISTIC rows of dataset
 NUM_ROWS_DETERMINISTIC = 5
 
-MAX_ROWS_TO_TAKE = 1000
+MAX_ROWS_TO_TAKE = 100000
