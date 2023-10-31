f08329bcd83843a0d531cbadb1551d493adc1b7f
Daniel Zhu <daniel.e.zhu+github@gmail.com>
2023-10-31 12:54:44 -0700

    fix: add qa accuracy semantic robustness to eval_algo_mapping.py (#83)
    Co-authored-by: Daniel Zhu <daniezh@amazon.com>

--- src/amazon_fmeval/eval_algo_mapping.py
+++ src/amazon_fmeval/eval_algo_mapping.py
@@ -9,6 +9,7 @@ from amazon_fmeval.eval_algorithms.factual_knowledge import FactualKnowledge
 from amazon_fmeval.eval_algorithms.general_semantic_robustness import GeneralSemanticRobustness
 from amazon_fmeval.eval_algorithms.prompt_stereotyping import PromptStereotyping
 from amazon_fmeval.eval_algorithms.qa_accuracy import QAAccuracy
+from amazon_fmeval.eval_algorithms.qa_accuracy_semantic_robustness import QAAccuracySemanticRobustness
 from amazon_fmeval.eval_algorithms.qa_toxicity import QAToxicity
 from amazon_fmeval.eval_algorithms.summarization_accuracy import SummarizationAccuracy
 from amazon_fmeval.eval_algorithms.classification_accuracy import ClassificationAccuracy
@@ -19,15 +20,16 @@ from amazon_fmeval.eval_algorithms.summarization_toxicity import SummarizationTo
 from amazon_fmeval.eval_algorithms.toxicity import Toxicity
 
 EVAL_ALGORITHMS: Dict[str, Type["EvalAlgorithmInterface"]] = {
-    EvalAlgorithm.FACTUAL_KNOWLEDGE.value: FactualKnowledge,
-    EvalAlgorithm.QA_ACCURACY.value: QAAccuracy,
-    EvalAlgorithm.SUMMARIZATION_ACCURACY.value: SummarizationAccuracy,
-    EvalAlgorithm.PROMPT_STEREOTYPING.value: PromptStereotyping,
     EvalAlgorithm.CLASSIFICATION_ACCURACY.value: ClassificationAccuracy,
-    EvalAlgorithm.GENERAL_SEMANTIC_ROBUSTNESS.value: GeneralSemanticRobustness,
-    EvalAlgorithm.SUMMARIZATION_ACCURACY_SEMANTIC_ROBUSTNESS.value: SummarizationAccuracySemanticRobustness,
-    EvalAlgorithm.TOXICITY.value: Toxicity,
-    EvalAlgorithm.QA_TOXICITY.value: QAToxicity,
-    EvalAlgorithm.SUMMARIZATION_TOXICITY.value: SummarizationToxicity,
     EvalAlgorithm.CLASSIFICATION_ACCURACY_SEMANTIC_ROBUSTNESS.value: ClassificationAccuracySemanticRobustness,
+    EvalAlgorithm.FACTUAL_KNOWLEDGE.value: FactualKnowledge,
+    EvalAlgorithm.GENERAL_SEMANTIC_ROBUSTNESS.value: GeneralSemanticRobustness,
+    EvalAlgorithm.PROMPT_STEREOTYPING.value: PromptStereotyping,
+    EvalAlgorithm.QA_ACCURACY.value: QAAccuracy,
+    EvalAlgorithm.QA_ACCURACY_SEMANTIC_ROBUSTNESS.value: QAAccuracySemanticRobustness,
+    EvalAlgorithm.QA_TOXICITY.value: QAToxicity,
+    EvalAlgorithm.SUMMARIZATION_ACCURACY.value: SummarizationAccuracy,
+    EvalAlgorithm.SUMMARIZATION_ACCURACY_SEMANTIC_ROBUSTNESS.value: SummarizationAccuracySemanticRobustness,
+    EvalAlgorithm.SUMMARIZATION_TOXICITY.value: SummarizationToxicity,
+    EvalAlgorithm.TOXICITY.value: Toxicity,
 }
