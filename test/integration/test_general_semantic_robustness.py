4743da277adbf161b4173f6e3e8a2dec33b6ebdd
Rahul Sahu <rvasahu@amazon.com>
2023-11-07 02:17:49 +0000

    change: use wikitext2 dataset
    documentation: remove stray comments

--- test/integration/test_general_semantic_robustness.py
+++ test/integration/test_general_semantic_robustness.py
@@ -1,11 +1,16 @@
 import os
+
+from copy import deepcopy
 from typing import NamedTuple, Dict
+
 
 import pytest
 from pytest import approx
-from amazon_fmeval.constants import MIME_TYPE_JSONLINES
 
-from amazon_fmeval.data_loaders.data_config import DataConfig
+from amazon_fmeval.eval_algorithms import (
+    DATASET_CONFIGS,
+    WIKITEXT2,
+)
 from amazon_fmeval.eval_algorithms.general_semantic_robustness import (
     BUTTER_FINGER,
     GeneralSemanticRobustness,
@@ -16,8 +21,7 @@ from amazon_fmeval.eval_algorithms.general_semantic_robustness import (
 )
 
 from test.integration.models.model_runners import (
-    sm_model_runner,  # use this or hf model runner?
-    sm_model_runner_prompt_template,
+    sm_model_runner,
 )
 
 ABS_TOL = 1e-4
@@ -27,9 +31,6 @@ os.environ["PARALLELIZATION_FACTOR"] = "2"
 class GSRTestCase(NamedTuple):
     config: GeneralSemanticRobustnessConfig
     expected_scores: Dict[str, float]
-
-
-# TODO: deep dive into how WER_SCORE is calculated
 
 
 class TestGeneralSemanticRobustness:
@@ -42,7 +43,7 @@ class TestGeneralSemanticRobustness:
                     num_perturbations=5,
                     butter_finger_perturbation_prob=0.1,
                 ),
-                expected_scores={WER_SCORE: 1.0},
+                expected_scores={WER_SCORE: 1.1},
             ),
             GSRTestCase(
                 config=GeneralSemanticRobustnessConfig(
@@ -50,7 +51,7 @@ class TestGeneralSemanticRobustness:
                     num_perturbations=5,
                     random_uppercase_corrupt_proportion=0.1,
                 ),
-                expected_scores={WER_SCORE: 2.0},
+                expected_scores={WER_SCORE: 0.26666666666666666},
             ),
             GSRTestCase(
                 config=GeneralSemanticRobustnessConfig(
@@ -59,7 +60,7 @@ class TestGeneralSemanticRobustness:
                     whitespace_remove_prob=0.1,
                     whitespace_add_prob=0.05,
                 ),
-                expected_scores={WER_SCORE: 1.0},
+                expected_scores={WER_SCORE: 0.5},
             ),
         ],
     )
@@ -68,9 +69,7 @@ class TestGeneralSemanticRobustness:
         model_input = "London is the capital of "
         eval_scores = gen_semantic_robustness.evaluate_sample(
             model_input=model_input,
-            model=sm_model_runner,  # use this or hf model runner?
-            # no target output? what about model output?
-            prompt_template=sm_model_runner_prompt_template,
+            model=sm_model_runner,
         )
         for eval_score in eval_scores:
             assert eval_score.value == gsr_test_case.expected_scores[eval_score.name]
@@ -84,7 +83,7 @@ class TestGeneralSemanticRobustness:
                     num_perturbations=5,
                     butter_finger_perturbation_prob=0.1,
                 ),
-                expected_scores={WER_SCORE: 1.1538023088023088},
+                expected_scores={WER_SCORE: 0.7579873015873015},
             ),
             GSRTestCase(
                 config=GeneralSemanticRobustnessConfig(
@@ -92,7 +91,7 @@ class TestGeneralSemanticRobustness:
                     num_perturbations=5,
                     random_uppercase_corrupt_proportion=0.1,
                 ),
-                expected_scores={WER_SCORE: 0.9224410774410775},
+                expected_scores={WER_SCORE: 0.5560531746031746},
             ),
             GSRTestCase(
                 config=GeneralSemanticRobustnessConfig(
@@ -101,23 +100,16 @@ class TestGeneralSemanticRobustness:
                     whitespace_remove_prob=0.1,
                     whitespace_add_prob=0.05,
                 ),
-                expected_scores={WER_SCORE: 0.8698067981401316},
+                expected_scores={WER_SCORE: 0.6135412698412699},
             ),
         ],
     )
-    def test_evaluate(self, integration_tests_dir, gsr_test_case):
+    def test_evaluate_full(self, gsr_test_case):
         gen_semantic_robustness = GeneralSemanticRobustness(gsr_test_case.config)
-        dataset_config = DataConfig(
-            dataset_name="triviaQA_sample",
-            dataset_uri=os.path.join(integration_tests_dir, "datasets", "triviaQA_sample.jsonl"),
-            dataset_mime_type=MIME_TYPE_JSONLINES,
-            model_input_location="question",
-            target_output_location="answer",
-        )
+        dataset_config = deepcopy(DATASET_CONFIGS[WIKITEXT2])
         eval_output = gen_semantic_robustness.evaluate(
             model=sm_model_runner,
             dataset_config=dataset_config,
-            prompt_template=sm_model_runner_prompt_template,
             save=True,
         )[0]
 
