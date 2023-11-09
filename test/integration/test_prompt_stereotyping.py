0f82f61a3cbde33fdb1d9235d44a11e43c49d07f
Rahul Sahu <rvasahu@amazon.com>
2023-11-09 19:11:33 +0000

    change: remove test cases with differing record numbers

--- test/integration/test_prompt_stereotyping.py
+++ test/integration/test_prompt_stereotyping.py
@@ -48,22 +48,6 @@ class TestPromptStereotyping:
         [
             PSTestCase(
                 model_runner=hf_model_runner,
-                num_records=100,
-                aggregate_scores={PROMPT_STEREOTYPING_SCORE: 0.6},
-                category_scores={
-                    "age": {PROMPT_STEREOTYPING_SCORE: 0.5},
-                    "disability": {PROMPT_STEREOTYPING_SCORE: 0.75},
-                    "gender": {PROMPT_STEREOTYPING_SCORE: 0.5},
-                    "nationality": {PROMPT_STEREOTYPING_SCORE: 0.7142857142857143},
-                    "physical-appearance": {PROMPT_STEREOTYPING_SCORE: 0.6},
-                    "race-color": {PROMPT_STEREOTYPING_SCORE: 0.5185185185185185},
-                    "religion": {PROMPT_STEREOTYPING_SCORE: 0.8461538461538461},
-                    "sexual-orientation": {PROMPT_STEREOTYPING_SCORE: 0.6},
-                    "socioeconomic": {PROMPT_STEREOTYPING_SCORE: 0.6},
-                },
-            ),
-            PSTestCase(
-                model_runner=hf_model_runner,
                 num_records=250,
                 aggregate_scores={PROMPT_STEREOTYPING_SCORE: 0.576},
                 category_scores={
@@ -76,22 +60,6 @@ class TestPromptStereotyping:
                     "religion": {PROMPT_STEREOTYPING_SCORE: 0.6296296296296297},
                     "sexual-orientation": {PROMPT_STEREOTYPING_SCORE: 0.7333333333333333},
                     "socioeconomic": {PROMPT_STEREOTYPING_SCORE: 0.6388888888888888},
-                },
-            ),
-            PSTestCase(
-                model_runner=hf_model_runner,
-                num_records=400,
-                aggregate_scores={PROMPT_STEREOTYPING_SCORE: 0.6125},
-                category_scores={
-                    "age": {PROMPT_STEREOTYPING_SCORE: 0.5},
-                    "disability": {PROMPT_STEREOTYPING_SCORE: 0.5789473684210527},
-                    "gender": {PROMPT_STEREOTYPING_SCORE: 0.5409836065573771},
-                    "nationality": {PROMPT_STEREOTYPING_SCORE: 0.5853658536585366},
-                    "physical-appearance": {PROMPT_STEREOTYPING_SCORE: 0.625},
-                    "race-color": {PROMPT_STEREOTYPING_SCORE: 0.6201550387596899},
-                    "religion": {PROMPT_STEREOTYPING_SCORE: 0.6666666666666666},
-                    "sexual-orientation": {PROMPT_STEREOTYPING_SCORE: 0.76},
-                    "socioeconomic": {PROMPT_STEREOTYPING_SCORE: 0.6458333333333334},
                 },
             ),
         ],
