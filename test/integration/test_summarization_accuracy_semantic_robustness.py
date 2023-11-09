130539eb36f22ae997d2bb3a07b0cdbc5068be12
Daniel Zhu <daniezh@amazon.com>
2023-11-09 00:31:09 -0800

    Refactor tests so that eval algo objects get reused as much as possible

--- test/integration/test_summarization_accuracy_semantic_robustness.py
+++ test/integration/test_summarization_accuracy_semantic_robustness.py
@@ -1,11 +1,11 @@
 import os
 import json
 import ray
-from typing import NamedTuple, Dict
-
 import pytest
+
+from typing import NamedTuple, Dict
 from pytest import approx
-from amazon_fmeval.eval_algorithms.summarization_accuracy_semantic_robustness import (
+from fmeval.eval_algorithms.summarization_accuracy_semantic_robustness import (
     SummarizationAccuracySemanticRobustness,
     SummarizationAccuracySemanticRobustnessConfig,
     ROUGE_SCORE,
@@ -18,42 +18,44 @@ from amazon_fmeval.eval_algorithms.summarization_accuracy_semantic_robustness im
     RANDOM_UPPER_CASE,
     WHITESPACE_ADD_REMOVE,
 )
-from amazon_fmeval.data_loaders.data_config import DataConfig
-from amazon_fmeval.constants import MIME_TYPE_JSONLINES
+from fmeval.data_loaders.data_config import DataConfig
+from fmeval.constants import MIME_TYPE_JSONLINES
 from test.integration.models.model_runners import sm_model_runner
 
 ABS_TOL = 1e-6
 os.environ["PARALLELIZATION_FACTOR"] = "2"
 
+BUTTER_FINGER_CONFIG = SummarizationAccuracySemanticRobustnessConfig(
+    perturbation_type=BUTTER_FINGER, num_perturbations=5, butter_finger_perturbation_prob=0.1
+)
+
+RANDOM_UPPER_CASE_CONFIG = SummarizationAccuracySemanticRobustnessConfig(
+    perturbation_type=RANDOM_UPPER_CASE,
+    num_perturbations=5,
+    random_uppercase_corrupt_proportion=0.1,
+)
+
+WHITESPACE_CONFIG = SummarizationAccuracySemanticRobustnessConfig(
+    perturbation_type=WHITESPACE_ADD_REMOVE,
+    num_perturbations=5,
+    whitespace_remove_prob=0.1,
+    whitespace_add_prob=0.05,
+)
+
+
+class TestCaseEvaluate(NamedTuple):
+    config: SummarizationAccuracySemanticRobustnessConfig
+    expected_evaluate_sample_scores: Dict[str, float]
+    expected_evaluate_scores: Dict[str, float]
+
 
 class TestSummarizationAccuracySemanticRobustness:
-    class TestCaseEvaluate(NamedTuple):
-        config: SummarizationAccuracySemanticRobustnessConfig
-        expected_scores: Dict[str, float]
-
-    BUTTER_FINGER_CONFIG = SummarizationAccuracySemanticRobustnessConfig(
-        perturbation_type=BUTTER_FINGER, num_perturbations=5, butter_finger_perturbation_prob=0.1
-    )
-
-    RANDOM_UPPER_CASE_CONFIG = SummarizationAccuracySemanticRobustnessConfig(
-        perturbation_type=RANDOM_UPPER_CASE,
-        num_perturbations=5,
-        random_uppercase_corrupt_proportion=0.1,
-    )
-
-    WHITESPACE_CONFIG = SummarizationAccuracySemanticRobustnessConfig(
-        perturbation_type=WHITESPACE_ADD_REMOVE,
-        num_perturbations=5,
-        whitespace_remove_prob=0.1,
-        whitespace_add_prob=0.05,
-    )
-
     @pytest.mark.parametrize(
-        "config, expected_scores",
+        "config, expected_evaluate_sample_scores, expected_evaluate_scores",
         [
             TestCaseEvaluate(
                 config=BUTTER_FINGER_CONFIG,
-                expected_scores={
+                expected_evaluate_sample_scores={
                     ROUGE_SCORE: 0.0,
                     METEOR_SCORE: 0.055555,
                     BERT_SCORE: 0.590943,
@@ -61,51 +63,7 @@ class TestSummarizationAccuracySemanticRobustness:
                     DELTA_METEOR_SCORE: 0.0,
                     DELTA_BERT_SCORE: -0.022468,
                 },
-            ),
-            TestCaseEvaluate(
-                config=RANDOM_UPPER_CASE_CONFIG,
-                expected_scores={
-                    ROUGE_SCORE: 0.0,
-                    METEOR_SCORE: 0.055555,
-                    BERT_SCORE: 0.590943,
-                    DELTA_ROUGE_SCORE: 0.0,
-                    DELTA_METEOR_SCORE: 0.011111,
-                    DELTA_BERT_SCORE: -0.015139,
-                },
-            ),
-            TestCaseEvaluate(
-                config=WHITESPACE_CONFIG,
-                expected_scores={
-                    ROUGE_SCORE: 0.0,
-                    METEOR_SCORE: 0.055555,
-                    BERT_SCORE: 0.590943,
-                    DELTA_ROUGE_SCORE: 0.0,
-                    DELTA_METEOR_SCORE: 0.016667,
-                    DELTA_BERT_SCORE: 0.009372,
-                },
-            ),
-        ],
-    )
-    def test_evaluate_sample(self, config, expected_scores, integration_tests_dir):
-        eval_algo = SummarizationAccuracySemanticRobustness(config)
-        with open(os.path.join(integration_tests_dir, "datasets", "xsum_sample.jsonl")) as fh:
-            json_obj = json.loads(fh.readline())
-            model_input = json_obj["document"]
-            target_output = json_obj["summary"]
-            eval_scores = eval_algo.evaluate_sample(
-                model_input=model_input,
-                model=sm_model_runner,
-                target_output=target_output,
-            )
-            for eval_score in eval_scores:
-                assert eval_score.value == approx(expected_scores[eval_score.name], abs=ABS_TOL)
-
-    @pytest.mark.parametrize(
-        "config, expected_scores",
-        [
-            TestCaseEvaluate(
-                config=BUTTER_FINGER_CONFIG,
-                expected_scores={
+                expected_evaluate_scores={
                     ROUGE_SCORE: 0.047974,
                     METEOR_SCORE: 0.085727,
                     BERT_SCORE: 0.571300,
@@ -116,7 +74,15 @@ class TestSummarizationAccuracySemanticRobustness:
             ),
             TestCaseEvaluate(
                 config=RANDOM_UPPER_CASE_CONFIG,
-                expected_scores={
+                expected_evaluate_sample_scores={
+                    ROUGE_SCORE: 0.0,
+                    METEOR_SCORE: 0.055555,
+                    BERT_SCORE: 0.590943,
+                    DELTA_ROUGE_SCORE: 0.0,
+                    DELTA_METEOR_SCORE: 0.011111,
+                    DELTA_BERT_SCORE: -0.015139,
+                },
+                expected_evaluate_scores={
                     ROUGE_SCORE: 0.047974,
                     METEOR_SCORE: 0.085727,
                     BERT_SCORE: 0.571300,
@@ -127,7 +93,15 @@ class TestSummarizationAccuracySemanticRobustness:
             ),
             TestCaseEvaluate(
                 config=WHITESPACE_CONFIG,
-                expected_scores={
+                expected_evaluate_sample_scores={
+                    ROUGE_SCORE: 0.0,
+                    METEOR_SCORE: 0.055555,
+                    BERT_SCORE: 0.590943,
+                    DELTA_ROUGE_SCORE: 0.0,
+                    DELTA_METEOR_SCORE: 0.016667,
+                    DELTA_BERT_SCORE: 0.009372,
+                },
+                expected_evaluate_scores={
                     ROUGE_SCORE: 0.047974,
                     METEOR_SCORE: 0.085727,
                     BERT_SCORE: 0.571300,
@@ -138,8 +112,29 @@ class TestSummarizationAccuracySemanticRobustness:
             ),
         ],
     )
-    def test_evaluate(self, integration_tests_dir, config, expected_scores):
+    def test_evaluate_sample_and_evaluate(
+        self, config, expected_evaluate_sample_scores, expected_evaluate_scores, integration_tests_dir
+    ):
+        """
+        In order to reuse eval algo objects (SummarizationAccuracySemanticRobustness)
+        as much as possible, we test evaluate_sample and evaluate back to back
+        using the same eval_algo.
+        """
         eval_algo = SummarizationAccuracySemanticRobustness(config)
+        # Test evaluate_sample()
+        with open(os.path.join(integration_tests_dir, "datasets", "xsum_sample.jsonl")) as fh:
+            json_obj = json.loads(fh.readline())
+            model_input = json_obj["document"]
+            target_output = json_obj["summary"]
+            eval_scores = eval_algo.evaluate_sample(
+                model_input=model_input,
+                model=sm_model_runner,
+                target_output=target_output,
+            )
+            for eval_score in eval_scores:
+                assert eval_score.value == approx(expected_evaluate_sample_scores[eval_score.name], abs=ABS_TOL)
+
+        # Test evaluate()
         dataset_config = DataConfig(
             dataset_name="xsum_sample",
             dataset_uri=os.path.join(integration_tests_dir, "datasets", "xsum_sample.jsonl"),
@@ -153,7 +148,7 @@ class TestSummarizationAccuracySemanticRobustness:
             save=True,
         )[0]
         for eval_score in eval_output.dataset_scores:
-            assert eval_score.value == approx(expected_scores[eval_score.name], abs=ABS_TOL)
+            assert eval_score.value == approx(expected_evaluate_scores[eval_score.name], abs=ABS_TOL)
 
     def test_ray_shutdown(self):
         """
