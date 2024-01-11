d36ef85e5b3a77a2239101b5448904517bae2160
Bilal Zafar <zafamuh@amazon.com>
2024-01-11 19:07:53 +0100

    Add integration tests for precision and recall

--- test/integration/test_qa_accuracy.py
+++ test/integration/test_qa_accuracy.py
@@ -6,6 +6,8 @@ from fmeval.eval_algorithms.qa_accuracy import (
     F1_SCORE,
     EXACT_MATCH_SCORE,
     QUASI_EXACT_MATCH_SCORE,
+    PRECISION,
+    RECALL,
 )
 
 from fmeval.data_loaders.data_config import DataConfig
@@ -60,3 +62,7 @@ class TestQAAccuracy:
                 assert eval_score.value == approx(0.060606, abs=ABS_TOL)
             elif eval_score.name == QUASI_EXACT_MATCH_SCORE:
                 assert eval_score.value == approx(0.303030, abs=ABS_TOL)
+            elif eval_score.name == PRECISION:
+                assert eval_score.value == approx(0.357660, abs=ABS_TOL)
+            elif eval_score.name == RECALL:
+                assert eval_score.value == approx(0.381313, abs=ABS_TOL)
