f5b34cda67cfb8977159b7044e8fb5e78ab6a8f9
Daniel Zhu <daniezh@amazon.com>
2024-02-15 09:48:19 -0800

    chore: remove XSUM dataset from example notebook and integration tests

--- test/integration/test_summarization_accuracy.py
+++ test/integration/test_summarization_accuracy.py
@@ -8,12 +8,13 @@ from fmeval.eval_algorithms.summarization_accuracy import (
     ROUGE_SCORE,
     BERT_SCORE,
 )
-from fmeval.data_loaders.data_config import DataConfig
-from fmeval.constants import MIME_TYPE_JSONLINES
+from fmeval.eval_algorithms import (
+    DATASET_CONFIGS,
+    GIGAWORD,
+)
 from test.integration.models.model_runners import bedrock_model_runner
 
-
-ABS_TOL = 1e-2  # Bedrock models are not deterministic, so we use a higher tolerance here
+ABS_TOL = 5e-2  # Bedrock models are not deterministic, so we use a higher tolerance here
 os.environ["PARALLELIZATION_FACTOR"] = "2"
 
 eval_algo = SummarizationAccuracy()
@@ -29,43 +30,32 @@ def format_input(input_str: str) -> str:
 
 class TestSummarizationAccuracy:
     def test_evaluate_sample(self, integration_tests_dir):
-        with open(os.path.join(integration_tests_dir, "datasets", "xsum_sample.jsonl")) as fh:
+        expected_scores = {METEOR_SCORE: 0.108, ROUGE_SCORE: 0.0, BERT_SCORE: 0.608}
+
+        with open(os.path.join(integration_tests_dir, "datasets", "gigaword_sample.jsonl")) as fh:
             json_obj = json.loads(fh.readline())
             original_text = json_obj["document"]
             target_output = json_obj["summary"]
             model_input = f"Summarise the following text in one sentence: {original_text}"
             model_output = bedrock_model_runner.predict(format_input(model_input))[0]
             eval_scores = eval_algo.evaluate_sample(target_output, model_output)
-            for eval_score in eval_scores:  # pragma: no branch
-                if eval_score.name == METEOR_SCORE:
-                    assert eval_score.value == approx(0.380, abs=ABS_TOL)
-                elif eval_score.name == ROUGE_SCORE:
-                    assert eval_score.value == approx(0.250, abs=ABS_TOL)
-                elif eval_score.name == BERT_SCORE:
-                    assert eval_score.value == approx(0.748, abs=ABS_TOL)
+            for eval_score in eval_scores:
+                assert eval_score.value == approx(expected_scores[eval_score.name], abs=ABS_TOL)
 
     def test_evaluate(self, integration_tests_dir):
-        dataset_config = DataConfig(
-            dataset_name="xsum_sample",
-            dataset_uri=os.path.join(integration_tests_dir, "datasets", "xsum_sample.jsonl"),
-            dataset_mime_type=MIME_TYPE_JSONLINES,
-            model_input_location="document",
-            target_output_location="summary",
-        )
+        expected_scores = {METEOR_SCORE: 0.317, ROUGE_SCORE: 0.060, BERT_SCORE: 0.632}
+        dataset_config = DATASET_CONFIGS[GIGAWORD]
         eval_outputs = eval_algo.evaluate(
             model=bedrock_model_runner,
             dataset_config=dataset_config,
             prompt_template=format_input("Summarise the following text in one sentence: $feature"),
             save=True,
+            num_records=20,
         )
         eval_output = eval_outputs[0]
-        for eval_score in eval_output.dataset_scores:
-            if eval_score.name == METEOR_SCORE:
-                assert eval_score.value == approx(0.279, abs=ABS_TOL)
-            elif eval_score.name == ROUGE_SCORE:
-                assert eval_score.value == approx(0.084, abs=ABS_TOL)
-            elif eval_score.name == BERT_SCORE:
-                assert eval_score.value == approx(0.677, abs=ABS_TOL)
+        eval_scores = eval_output.dataset_scores
+        for eval_score in eval_scores:
+            assert eval_score.value == approx(expected_scores[eval_score.name], abs=ABS_TOL)
 
     def test_ray_shutdown(self):
         """
