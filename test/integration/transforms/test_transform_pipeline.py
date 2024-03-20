520d510677f883f1e9241cc69955fd1f7a81d2be
Daniel Zhu <daniel.e.zhu+github@gmail.com>
2024-03-20 11:05:18 -0700

    feat: update GetModelResponse transform to support multiple model invocations on the same input (#220)

--- test/integration/transforms/test_transform_pipeline.py
+++ test/integration/transforms/test_transform_pipeline.py
@@ -22,7 +22,7 @@ def test_pipeline_execution():
     )
 
     get_model_response = GetModelResponse(
-        input_to_output_keys={gen_prompt.output_keys[0]: ["model_output"]},
+        input_key_to_response_keys={gen_prompt.output_keys[0]: [("model_output",)]},
         model_runner=sm_model_runner,
     )
 
--- test/unit/transforms/test_common.py
+++ test/unit/transforms/test_common.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import patch
-from typing import NamedTuple, List, Optional, Dict, Any
+from typing import NamedTuple, List, Optional, Dict, Any, Tuple
 
 from fmeval.transforms.common import GeneratePrompt, GetModelResponse
 from fmeval.util import EvalAlgorithmInternalError
@@ -46,7 +46,7 @@ def test_get_model_response_init_success():
     """
     with patch("fmeval.transforms.common.ModelRunner") as mock_model_runner:
         get_model_response = GetModelResponse(
-            input_to_output_keys={"prompt": ["model_output"]}, model_runner=mock_model_runner
+            input_key_to_response_keys={"prompt": [("model_output",)]}, model_runner=mock_model_runner
         )
         assert get_model_response.input_keys == ["prompt"]
         assert get_model_response.output_keys == ["model_output"]
@@ -56,34 +56,34 @@ def test_get_model_response_init_success():
 class TestCaseGetModelResponseSuccess(NamedTuple):
     model_output: Optional[str]
     log_prob: Optional[float]
-    output_keys: List[str]
+    response_keys: List[Tuple[str]]
     expected_result: Dict[str, Any]
 
 
 @pytest.mark.parametrize(
-    "model_output, log_prob, output_keys, expected_result",
+    "model_output, log_prob, response_keys, expected_result",
     [
         TestCaseGetModelResponseSuccess(
             model_output="some output",
             log_prob=-0.162,
-            output_keys=["model_output", "log_prob"],
+            response_keys=[("model_output", "log_prob")],
             expected_result={"input": "Hello", "model_output": "some output", "log_prob": -0.162},
         ),
         TestCaseGetModelResponseSuccess(
             model_output="some output",
             log_prob=None,
-            output_keys=["model_output"],
+            response_keys=[("model_output",)],
             expected_result={"input": "Hello", "model_output": "some output"},
         ),
         TestCaseGetModelResponseSuccess(
             model_output=None,
             log_prob=-0.162,
-            output_keys=["log_prob"],
+            response_keys=[("log_prob",)],
             expected_result={"input": "Hello", "log_prob": -0.162},
         ),
     ],
 )
-def test_get_model_response_call_success(model_output, log_prob, output_keys, expected_result):
+def test_get_model_response_call_success(model_output, log_prob, response_keys, expected_result):
     """
     GIVEN a GetModelResponse instance.
     WHEN its __call__ method is called on a record.
@@ -92,7 +92,7 @@ def test_get_model_response_call_success(model_output, log_prob, output_keys, ex
     with patch("fmeval.transforms.common.ModelRunner") as mock_model_runner:
         mock_model_runner.predict.return_value = (model_output, log_prob)
         get_model_response = GetModelResponse(
-            input_to_output_keys={"input": output_keys}, model_runner=mock_model_runner
+            input_key_to_response_keys={"input": response_keys}, model_runner=mock_model_runner
         )
         sample = {"input": "Hello"}
         result = get_model_response(sample)
@@ -106,11 +106,11 @@ def test_get_model_response_call_multiple_inputs():
     THEN the correct output is returned.
     """
     with patch("fmeval.transforms.common.ModelRunner") as mock_model_runner:
-        mock_model_runner.predict.side_effect = [("output 1", -0.162), ("output 2", -0.189)]
+        mock_model_runner.predict.side_effect = [("output 1", -0.162), ("output 2", -0.189), ("output 3", -0.126)]
         get_model_response = GetModelResponse(
-            input_to_output_keys={
-                "input_1": ["output_key_1", "log_prob_key_1"],
-                "input_2": ["output_key_2", "log_prob_key_2"],
+            input_key_to_response_keys={
+                "input_1": [("output_key_1", "log_prob_key_1"), ("output_key_2", "log_prob_key_2")],
+                "input_2": [("output_key_3", "log_prob_key_3")],
             },
             model_runner=mock_model_runner,
         )
@@ -122,6 +122,8 @@ def test_get_model_response_call_multiple_inputs():
             "input_2": "input 2",
             "output_key_2": "output 2",
             "log_prob_key_2": -0.189,
+            "output_key_3": "output 3",
+            "log_prob_key_3": -0.126,
         }
         result = get_model_response(sample)
         assert result == expected_result
@@ -130,30 +132,30 @@ def test_get_model_response_call_multiple_inputs():
 class TestCaseGetModelResponseFailure(NamedTuple):
     model_output: Optional[str]
     log_prob: Optional[float]
-    output_keys: List[str]
+    response_keys: List[Tuple[str]]
 
 
 @pytest.mark.parametrize(
-    "model_output, log_prob, output_keys",
+    "model_output, log_prob, response_keys",
     [
         TestCaseGetModelResponseFailure(
             model_output="some output",
             log_prob=-0.162,
-            output_keys=["model_output"],
+            response_keys=[("model_output",)],
         ),
         TestCaseGetModelResponseFailure(
             model_output="some output",
             log_prob=None,
-            output_keys=["model_output", "log_prob"],
+            response_keys=[("model_output", "log_prob")],
         ),
         TestCaseGetModelResponseFailure(
             model_output=None,
             log_prob=-0.162,
-            output_keys=["model_output", "log_prob"],
+            response_keys=[("model_output", "log_prob")],
         ),
     ],
 )
-def test_get_model_response_call_failure(model_output, log_prob, output_keys):
+def test_get_model_response_call_failure(model_output, log_prob, response_keys):
     """
     GIVEN a GetModelResponse instance where the number of output keys corresponding to
         a particular input key does not match the number of non-null elements in its model runner's
@@ -165,7 +167,7 @@ def test_get_model_response_call_failure(model_output, log_prob, output_keys):
     with patch("fmeval.transforms.common.ModelRunner") as mock_model_runner:
         mock_model_runner.predict.return_value = (model_output, log_prob)
         get_model_response = GetModelResponse(
-            input_to_output_keys={"input": output_keys},
+            input_key_to_response_keys={"input": response_keys},
             model_runner=mock_model_runner,
         )
         with pytest.raises(EvalAlgorithmInternalError, match="The number of elements in model response"):
