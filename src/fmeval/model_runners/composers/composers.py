d3e13d06f3ba7f565934eba28ed41cb0183ac370
Xiaoyi Cheng <xiayche@amazon.com>
2024-05-07 05:57:35 -0700

    feat: allow keyword arguments for prompt composer

--- src/fmeval/model_runners/composers/composers.py
+++ src/fmeval/model_runners/composers/composers.py
@@ -1,6 +1,6 @@
 import json
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Union, List
+from typing import Any, Dict, Union, List, Optional
 from fmeval.exceptions import EvalAlgorithmClientError
 from fmeval.model_runners.composers.template import VanillaTemplate
 
@@ -16,22 +16,28 @@ class Composer(ABC):
         self.placeholder = placeholder
         self.vanilla_template = VanillaTemplate(template)
 
-    def _get_filled_in_template(self, data: str) -> str:
+    def _get_filled_in_template(self, data: Optional[str], **kwargs) -> str:
         """
         Returns the string that results from replacing self.placeholder
-        in self.template with `data`.
+        in self.template with `data`. When both data and kwargs are given and there are duplicates,
+        the placeholders from kwargs take precedence.
 
         :param data: Data to replace placeholder.
         :return: A template that has its placeholders "filled in".
         """
-        return self.vanilla_template.substitute(**{self.placeholder: data})
+        mapping_obj = {}
+        if data:
+            mapping_obj = {self.placeholder: data}
+        mapping_obj.update(**kwargs)
+        return self.vanilla_template.substitute(**mapping_obj)
 
     @abstractmethod
-    def compose(self, data: str) -> Any:
+    def compose(self, data: Optional[str], **kwargs) -> Any:
         """
         Composes an object using the input data, self.vanilla_template, and self.placeholder.
 
         :param data: The data used to compose a new object.
+        :param **kwargs: Arbitrary keyword arguments used for composing.
         :return: A new object composed using `data`, self.vanilla_template, and self.placeholder.
         """
 
@@ -78,16 +84,20 @@ class PromptComposer(Composer):
     def __init__(self, template: str):
         super().__init__(template=template, placeholder=self.PLACEHOLDER)
 
-    def compose(self, data: str) -> str:
+    def compose(self, data: Optional[str] = None, **kwargs: Any) -> str:
         """
-        Composes a prompt that will be fed to an LLM.
+        Composes a prompt with data and/or from keyword arguments that will be fed to an LLM.
         Example:
             data = "London is the capital of"
+            template =
+                "<s>[INST] <<SYS>>Answer the following question in as few words as possible.<</SYS>>
+                Question: $model_input [/INST]"
             composed prompt =
                 "<s>[INST] <<SYS>>Answer the following question in as few words as possible.<</SYS>>
                 Question: London is the capital of [/INST]"
 
         :param data: The original string that forms the basis of the returned prompt.
+        :param **kwargs: Keyword arguments that used for filling in template.
         :return: A prompt composed by replacing self.placeholder in self.vanilla_template with `data`.
         """
-        return self._get_filled_in_template(data)
+        return self._get_filled_in_template(data, **kwargs)
--- test/unit/model_runners/composers/test_composers.py
+++ test/unit/model_runners/composers/test_composers.py
@@ -1,5 +1,5 @@
 import re
-from typing import NamedTuple, Union, List, Dict
+from typing import NamedTuple, Union, List, Dict, Optional
 
 import pytest
 
@@ -52,12 +52,49 @@ class TestJsonContentComposer:
 
 
 class TestPromptComposer:
-    def test_compose(self):
-        composer = PromptComposer(template="Answer the following question: $model_input")
-        prompt = "London is the capital of?"
-        expected_result = "Answer the following question: London is the capital of?"
-        result = composer.compose(prompt)
-        assert result == expected_result
+    class TestCaseCompose(NamedTuple):
+        template: str
+        prompt: Optional[str]
+        kwargs: Dict
+        expected_result: str
+
+    @pytest.mark.parametrize(
+        "test_case",
+        [
+            # Test case to verify composing a prompt with `data`
+            TestCaseCompose(
+                template="Answer the following question: $model_input",
+                prompt="London is the capital of?",
+                kwargs={},
+                expected_result="Answer the following question: London is the capital of?",
+            ),
+            # Test case verify composing a prompt with multiple keyword arguments
+            TestCaseCompose(
+                template="Question: $model_input \n context: $context \n statement: $statements",
+                prompt=None,
+                kwargs={"model_input": "sample question", "context": "sample context", "statements": "statement1"},
+                expected_result="Question: sample question \n context: sample context \n statement: statement1",
+            ),
+            # Test case verify composing a prompt with keyword argument takes higher priority
+            TestCaseCompose(
+                template="Question: $model_input",
+                prompt="question from prompt",
+                kwargs={"model_input": "question from kwargs"},
+                expected_result="Question: question from kwargs",
+            ),
+            # Test case verify composing a prompt with both `data` and keyword arguments
+            TestCaseCompose(
+                template="Question: $model_input \n Context: $context",
+                prompt="question from prompt",
+                kwargs={"context": "some context"},
+                expected_result="Question: question from prompt \n Context: some context",
+            ),
+        ],
+    )
+    def test_compose(self, test_case):
+        composer = PromptComposer(template=test_case.template)
+        result = composer.compose(test_case.prompt, **test_case.kwargs)
+        assert result == test_case.expected_result
 
     def test_invalid_template(self):
         composer = PromptComposer(template="Answer the following question: $invalid")
