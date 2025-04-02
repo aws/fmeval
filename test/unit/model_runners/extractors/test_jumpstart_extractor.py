fb99d3c6a72af24e4853a0ea7d8abfe84b5e6c46
Bikash Shrestha <shgbikas@amazon.com>
2025-04-02 13:59:40 -0700

    remove unnecessary description

--- test/unit/model_runners/extractors/test_jumpstart_extractor.py
+++ test/unit/model_runners/extractors/test_jumpstart_extractor.py
@@ -236,11 +236,6 @@ class TestJumpStartExtractor:
 
     @patch("sagemaker.session.Session")
     def test_extractor_when_default_payloads_found_outside_inference_configs(self, sagemaker_session):
-        """
-        GIVEN the default payloads data that is found is empty.
-        WHEN a JumpStartExtractor is being initialized.
-        THEN the correct exception is raised.
-        """
         sagemaker_session.boto_region_name = "us-west-2"
         model_spec_json = {"default_payloads": {"test": {"output_keys": {"generated_text": "Hi"}}}}
 
